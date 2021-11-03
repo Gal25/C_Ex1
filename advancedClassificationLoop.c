8e7f9c8a307c6b21f8ce8e2cc7db9ecbb229fbbb
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-03 18:36:47 +0200

    Add files via upload

---
+++ advancedClassificationLoop.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+
+int isPalindrome(int num){
+    int rev_num =0;
+    int ans= num;
+    while (num > 0){
+        rev_num = rev_num*10 + num%10;
+        num= num/10;
+    }
+     if (rev_num == ans){return 1;}
+     else{return 0; }
+     
+}
+int isArmstrong(int num){
+   int ans=0;
+   int temp=0;
+   int n = 0;
+   int sum = 0;
+   
+ for (ans = num; ans != 0; ++n) {
+       ans /= 10;
+   }
+
+   for (ans= num; ans != 0; ans /= 10) {
+       temp=ans % 10;
+      sum += pow(temp, n);
+   }
+
+  
+   if (sum == num){return 1;}
+else{return 0;}
+}
