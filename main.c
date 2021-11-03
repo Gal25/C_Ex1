24f60eda57ab3b784e2ba44bf016fd595a3cf56b
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-03 18:55:30 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+#include "basicClassification.c"
+#include "advancedClassificationLoop.c"
+
+void main(){
+
+     int num1,num2;
+     printf("Please enter number 1: ");
+     scanf("%d",&num1);
+     printf("Please enter number 2: ");
+     scanf("%d",&num2);
+         
+        if (num2 < num1){
+        int temp= num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    printf("The Armstrong numbers are:");
+     for(int i=num1;i<=num2;i++){
+         int ans=isArmstrong(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
+
+
+      printf("\n");
+      printf("The Palindromes are:");
+     for(int i=num1;i<=num2;i++){
+         int ans=isPalindrome(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
+
+     printf("\n");
+     printf("The Prime numbers are:");
+     for(int i=num1; i<=num2; i++){
+         int ans=isPrime(i);
+         if(ans==1) {
+             printf(" %d",i);
+         }
+     }
+    
+      printf("\n");
+      printf("The Strong numbers are:");
+     for(int i=num1; i<=num2; i++){
+         int ans=isStrong(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
+    printf("\n");
+
+     
+ }
