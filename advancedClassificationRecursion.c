cfd81290fec9f0b5ebb8835f2013b01502d47922
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-03 18:37:30 +0200

    Add files via upload

---
+++ advancedClassificationRecursion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+
+int order(int x);
+int power(int x, unsigned int y);
+int isArmstrong(int num ){
+    int n = order(num);
+    int temp = num, sum = 0;
+    while (temp!=0) {
+        int r = temp % 10;
+        sum += power(r, n);
+        temp = temp / 10;
+    }
+ 
+    if (sum == num)
+        return 1;
+    else
+        return 0;
+}
+int power(int x, unsigned int y){
+    if (y == 0)
+        return 1;
+    if (y % 2 == 0)
+        return power(x, y / 2) * power(x, y / 2);
+    return x * power(x, y / 2) * power(x, y / 2);
+}
+ 
+int order(int x){
+    int n = 0;
+    while (x!=0) {
+        x = x / 10;
+        n++;
+       
+    }
+    return n;
+}
+
+int rev_num (int num, int rev);
+int isPalindrome(int num){
+    int reverse= rev_num(num, 0);
+    if(reverse == num){
+        return 1; 
+    }
+    return 0;
+}
+
+int rev_num (int num, int rev){
+    if(num==0){return rev;}
+    rev= rev*10 + num%10;
+    return rev_num(num/10, rev);
+}
