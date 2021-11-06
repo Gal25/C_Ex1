66d7e42e38cbb15b03641a23d82062db4944fd04
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-06 10:49:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
+#include <math.h>
 #include "NumClass.h"
 
-int main() {
-    int start = 0, end = 0, i;
-//    printf("Enter the first number: \n");
-    scanf("%d", &start);
-    scanf("%d", &end);
+
+int main(){
+
+   int num1,num2;
+     //printf("Please enter 2 numbers: ");
+     scanf("%d %d",&num1 , &num2);
+         
+        if (num2 < num1){
+        int temp= num2;
+        num2 = num1;
+        num1 = temp;
+    }
     printf("The Armstrong numbers are:");
-    for(i = start; i <= end; i++ ){
-        if(isArmstrong(i)){
-            printf(" %d", i);
-        }
-    }
+     for(int i=num1;i<=num2;i++){
+         int ans=isArmstrong(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
+
+
+      printf("\n");
+      printf("The Palindromes are:");
+     for(int i=num1;i<=num2;i++){
+         int ans=isPalindrome(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
+
+     printf("\n");
+     printf("The Prime numbers are:");
+     for(int i=num1; i<=num2; i++){
+         int ans=isPrime(i);
+         if(ans==1) {
+             printf(" %d",i);
+         }
+     }
+    
+      printf("\n");
+      printf("The Strong numbers are:");
+     for(int i=num1; i<=num2; i++){
+         int ans=isStrong(i);
+         if(ans==1)
+         printf(" %d",i);
+     }
     printf("\n");
-    printf("The Palindromes are:");
-    for(i = start; i <= end; i++ ){
-        if(isPalindrome(i)){
-            printf(" %d", i);
-        }
-    }
     printf("\n");
-    printf("The Prime numbers are:");
-    for(i = start; i <= end; i++ ){
-        if(isPrime(i)){
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
-    printf("The Strong numbers are:");
-    for(i = start; i <= end; i++ ){
-        if(isStrong(i)){
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
-    return 0;
-}
+     
+     return 0;
+
+     
+ }
