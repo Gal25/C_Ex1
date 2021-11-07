d9c378e8576145d6c1204cd7ed8d5bd232b8842e
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-07 09:55:39 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int rev_num (int num, int rev);
+int PalindromeHelper (int num, int rev);
 int isPalindrome(int num){
-    int reverse= rev_num(num, 0);
+    int reverse= PalindromeHelper(num, 0);
     if(reverse == num){
         return 1; 
     }
     return 0;
 }
 
-int rev_num (int num, int rev){
+int PalindromeHelper (int num, int rev){
     if(num==0){return rev;}
     rev= rev*10 + num%10;
-    return rev_num(num/10, rev);
+    return PalindromeHelper(num/10, rev);
 }
 
 int NumOfDigit(int num);
